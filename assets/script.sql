cdb60cb004bac48d648644c621d5a835f52b9e36
William Severino <william@willcodes.com>
2022-02-14 14:36:25 -0300

    Modified script.sql to include primary key. Adding some images do be used on README.md

--- assets/script.sql
+++ assets/script.sql
@@ -1,4 +1,4 @@
 create schema chat;
 use chat;
-create table messages(`datetime` TIMESTAMP, `from` varchar(100), `message` varchar(500));
+create table messages(`MessageId` int NOT NULL AUTO_INCREMENT, `datetime` TIMESTAMP, `from` varchar(100), `message` varchar(500), PRIMARY KEY (MessageId));
 create index ix_datetime on messages(`datetime`);
